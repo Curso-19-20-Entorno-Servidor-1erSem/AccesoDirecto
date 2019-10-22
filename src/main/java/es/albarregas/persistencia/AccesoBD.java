e3753b2338168550723d0f6b8e3f83842134e30a
franciscoantonio <franciscoantonio@paquito-Lenovo-Ubuntu>
2019-10-22 02:23:46 +0200

    Actualización AvesFranciscoAMP.
    Se rectifica nombre del beans ave.java por Ave.java y se crea constructor y set & gets.
    
    Se crea conexión a la Base de Datos en el método doPost del Servlet AccesoBD.java
    
    22/10/19 02:22:35 
    by Francisco Antonio Murillo Pacheco

--- src/main/java/es/albarregas/persistencia/AccesoBD.java
+++ src/main/java/es/albarregas/persistencia/AccesoBD.java
@@ -5,13 +5,27 @@
  */
 package es.albarregas.persistencia;
 
+
+
+import es.albarregas.beans.Ave;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  *
@@ -32,6 +46,7 @@ public class AccesoBD extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
@@ -72,17 +87,99 @@ public class AccesoBD extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+         processRequest(request, response);
+        Connection conexion = null;
+        Statement sentencia = null;
+        PreparedStatement preparada = null;
+        ResultSet resultado = null;
+        Ave ave = null;
+        List<Ave> listado = null;
+
+        String anilla = request.getParameter("anilla");
+        String sql = null;
+        String url = null;
+        
+
+        
+        
+        
+        try {
+            //Carga el controlador para acceder a la BD.
+            Class.forName("com.mysql.jdbc.Driver");
+            //Realiza la conexión.
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebasjava","java2019","2019");
+            sentencia = conexion.createStatement();
+            
+            if (request.getParameter("unaAnilla") != null) {
+
+                sql = "select * from aves where anilla = ?";
+                preparada = conexion.prepareStatement(sql);
+                preparada.setString(1, anilla);
+                try {
+                    resultado = preparada.executeQuery();
+
+                    resultado.next();
+                    url = "unResultado.jsp";
+                    request.setAttribute("anilla", resultado.getString("anilla"));
+                    request.setAttribute("especie", resultado.getString("especie"));
+                    request.setAttribute("lugar", resultado.getString("lugar"));
+                    request.setAttribute("fecha", resultado.getString("fecha"));
+                } catch (SQLException e) {
+
+                    url = "error.jsp";
+                    request.setAttribute("error", "La anilla " + anilla
+                            + " no existe en la base de datos");
+                }
+            } else {
+                sql = "select * from aves";
+                sentencia = conexion.createStatement();
+                resultado = sentencia.executeQuery(sql);
+                listado = new ArrayList();
+                url = "listaResultado.jsp";
+
+                while (resultado.next()) {
+
+                    ave = new Ave();
+                    ave.setAnilla(resultado.getString("anilla"));
+                    ave.setEspecie(resultado.getString("especie"));
+                    ave.setLugar(resultado.getString("lugar"));
+                    ave.setFecha(resultado.getString("fecha"));
+                    listado.add(ave);
+                }
+                request.setAttribute("lista", listado);
+            }
+
+            request.getRequestDispatcher(url).forward(request, response);
+        } catch (SQLException ex) {
+            System.out.println("Error al crear la conexión");
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (resultado != null) {
+                    resultado.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            try {
+                if (preparada != null) {
+                    preparada.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            try {
+                if (conexion != null) {
+                    conexion.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+
 
 }
